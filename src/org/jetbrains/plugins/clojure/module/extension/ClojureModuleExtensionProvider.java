c965c25357e82796251a7dc973794f16fefc88a3
VISTALL <vistall.valeriy@gmail.com>
2014-01-27 20:16:36 +0200

    to new api

--- src/org/jetbrains/plugins/clojure/module/extension/ClojureModuleExtensionProvider.java
+++ src/org/jetbrains/plugins/clojure/module/extension/ClojureModuleExtensionProvider.java
@@ -12,24 +12,33 @@ import com.intellij.openapi.module.Module;
  * @author VISTALL
  * @since 14:38/12.06.13
  */
-public class ClojureModuleExtensionProvider implements ModuleExtensionProvider<ClojureModuleExtension, ClojureMutableModuleExtension> {
-  @Nullable
-  public Icon getIcon() {
-    return ClojureIcons.CLOJURE_ICON_16x16;
-  }
+public class ClojureModuleExtensionProvider implements ModuleExtensionProvider<ClojureModuleExtension, ClojureMutableModuleExtension>
+{
+	@Override
+	@Nullable
+	public Icon getIcon()
+	{
+		return ClojureIcons.CLOJURE_ICON_16x16;
+	}
 
-  @NotNull
-  public String getName() {
-    return "Clojure";
-  }
+	@Override
+	@NotNull
+	public String getName()
+	{
+		return "Clojure";
+	}
 
-  @NotNull
-  public ClojureModuleExtension createImmutable(@NotNull String s, @NotNull Module module) {
-    return new ClojureModuleExtension(s, module);
-  }
+	@Override
+	@NotNull
+	public ClojureModuleExtension createImmutable(@NotNull String s, @NotNull Module module)
+	{
+		return new ClojureModuleExtension(s, module);
+	}
 
-  @NotNull
-  public ClojureMutableModuleExtension createMutable(@NotNull String s, @NotNull Module module, @NotNull ClojureModuleExtension clojureModuleExtension) {
-    return new ClojureMutableModuleExtension(s, module, clojureModuleExtension);
-  }
+	@Override
+	@NotNull
+	public ClojureMutableModuleExtension createMutable(@NotNull String s, @NotNull Module module)
+	{
+		return new ClojureMutableModuleExtension(s, module);
+	}
 }
--- src/org/jetbrains/plugins/clojure/module/extension/ClojureMutableModuleExtension.java
+++ src/org/jetbrains/plugins/clojure/module/extension/ClojureMutableModuleExtension.java
@@ -1,63 +1,62 @@
 package org.jetbrains.plugins.clojure.module.extension;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.roots.ModifiableRootModel;
-import com.intellij.openapi.util.Comparing;
+import javax.swing.JComponent;
+
 import org.consulo.module.extension.MutableModuleExtension;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.clojure.config.ui.ClojureFacetSettingsTab;
-
-import javax.swing.*;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.util.Comparing;
 
 /**
  * @author VISTALL
  * @since 14:37/12.06.13
  */
-public class ClojureMutableModuleExtension extends ClojureModuleExtension implements MutableModuleExtension<ClojureModuleExtension> {
-  private final ClojureModuleExtension myModuleExtension;
+public class ClojureMutableModuleExtension extends ClojureModuleExtension implements MutableModuleExtension<ClojureModuleExtension>
+{
+	public ClojureMutableModuleExtension(@NotNull String id, @NotNull Module module)
+	{
+		super(id, module);
+	}
 
-  public ClojureMutableModuleExtension(@NotNull String id, @NotNull Module module, ClojureModuleExtension clojureModuleExtension) {
-    super(id, module);
-    myModuleExtension = clojureModuleExtension;
-    commit(clojureModuleExtension);
-  }
+	public void setReplClass(@NotNull String replClass)
+	{
+		myReplClass = replClass;
+	}
 
-  public void setReplClass(@NotNull String replClass) {
-    myReplClass = replClass;
-  }
+	public void setJvmOpts(@NotNull String jvmOpts)
+	{
+		myJvmOpts = jvmOpts;
+	}
 
-  public void setJvmOpts(@NotNull String jvmOpts) {
-    myJvmOpts = jvmOpts;
-  }
+	public void setReplOpts(@NotNull String replOpts)
+	{
+		myReplOpts = replOpts;
+	}
 
-  public void setReplOpts(@NotNull String replOpts) {
-    myReplOpts = replOpts;
-  }
+	@Nullable
+	@Override
+	public JComponent createConfigurablePanel(@NotNull ModifiableRootModel modifiableRootModel, @Nullable Runnable runnable)
+	{
+		return new ClojureFacetSettingsTab(this);
+	}
 
-  @Nullable
-  @Override
-  public JComponent createConfigurablePanel(@NotNull ModifiableRootModel modifiableRootModel, @Nullable Runnable runnable) {
-    return new ClojureFacetSettingsTab(this);
-  }
+	@Override
+	public void setEnabled(boolean b)
+	{
+		myIsEnabled = b;
+	}
 
-  @Override
-  public void setEnabled(boolean b) {
-    myIsEnabled = b;
-  }
-
-  @Override
-  public boolean isModified() {
-    boolean modified = false;
-    modified |= isEnabled() != myModuleExtension.isEnabled();
-    modified |= !Comparing.equal(getJvmOpts(), myModuleExtension.getJvmOpts());
-    modified |= !Comparing.equal(getReplClass(), myModuleExtension.getReplClass());
-    modified |= !Comparing.equal(getReplOpts(), myModuleExtension.getReplOpts());
-    return modified;
-  }
-
-  @Override
-  public void commit() {
-    myModuleExtension.commit(this);
-  }
+	@Override
+	public boolean isModified(@NotNull ClojureModuleExtension extension)
+	{
+		boolean modified = false;
+		modified |= isEnabled() != extension.isEnabled();
+		modified |= !Comparing.equal(getJvmOpts(), extension.getJvmOpts());
+		modified |= !Comparing.equal(getReplClass(), extension.getReplClass());
+		modified |= !Comparing.equal(getReplOpts(), extension.getReplOpts());
+		return modified;
+	}
 }
