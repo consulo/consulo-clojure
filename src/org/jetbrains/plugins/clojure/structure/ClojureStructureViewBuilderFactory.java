0d8b61aa8d05d5c1867bf7c83331a28ed0cf7734
VISTALL <vistall.valeriy@gmail.com>
2014-02-09 01:27:08 +0200

    compilation fixed

--- src/org/jetbrains/plugins/clojure/structure/ClojureStructureViewBuilderFactory.java
+++ src/org/jetbrains/plugins/clojure/structure/ClojureStructureViewBuilderFactory.java
@@ -1,26 +1,35 @@
 package org.jetbrains.plugins.clojure.structure;
 
-import com.intellij.lang.PsiStructureViewFactory;
-import com.intellij.ide.structureView.StructureViewBuilder;
-import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
-import com.intellij.ide.structureView.StructureViewModel;
-import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
+import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
 
 /**
  * @author ilyas
  */
-public class ClojureStructureViewBuilderFactory implements PsiStructureViewFactory {
-    public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-        return new TreeBasedStructureViewBuilder() {
-            @NotNull
-            public StructureViewModel createStructureViewModel() {
-                return new ClojureStructureViewModel(psiFile);
-            }
+public class ClojureStructureViewBuilderFactory implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@Override
+			@NotNull
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new ClojureStructureViewModel(psiFile);
+			}
 
-            public boolean isRootNodeShown() {
-                return false;
-            }
-        };
-    }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
