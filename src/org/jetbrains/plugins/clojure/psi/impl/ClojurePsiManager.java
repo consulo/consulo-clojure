4ebc4eca0a6505f5e00500016d6ff2754ea6303b
VISTALL <vistall.valeriy@gmail.com>
2015-02-27 19:39:29 +0300

    fixed #CEP-542

--- src/org/jetbrains/plugins/clojure/psi/impl/ClojurePsiManager.java
+++ src/org/jetbrains/plugins/clojure/psi/impl/ClojurePsiManager.java
@@ -1,50 +1,64 @@
 package org.jetbrains.plugins.clojure.psi.impl;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.clojure.file.ClojureFileType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.startup.StartupManager;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.clojure.psi.stubs.ClojureShortNamesCache;
-import org.jetbrains.plugins.clojure.file.ClojureFileType;
+import com.intellij.psi.PsiFileFactory;
 
 /**
  * @author ilyas
  */
-public class ClojurePsiManager implements ProjectComponent {
-  private final Project myProject;
-  private PsiFile myDummyFile;
+public class ClojurePsiManager implements ProjectComponent
+{
+	private final Project myProject;
+	private PsiFile myDummyFile;
 
-  public ClojurePsiManager(Project project) {
-    myProject = project;
-  }
+	public ClojurePsiManager(Project project)
+	{
+		myProject = project;
+	}
 
-  public void projectOpened() {
-  }
+	public void projectOpened()
+	{
+	}
 
-  public void projectClosed() {
-  }
+	public void projectClosed()
+	{
+	}
 
-  @NotNull
-  public String getComponentName() {
-    return "ClojurePsiManager";
-  }
+	@NotNull
+	public String getComponentName()
+	{
+		return "ClojurePsiManager";
+	}
 
-  public void initComponent() {
-    myDummyFile = PsiFileFactory.getInstance(myProject).createFileFromText("dummy." + ClojureFileType.CLOJURE_FILE_TYPE.getDefaultExtension(), "");
-  }
+	public void initComponent()
+	{
+		ApplicationManager.getApplication().runReadAction(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				myDummyFile = PsiFileFactory.getInstance(myProject).createFileFromText("dummy." + ClojureFileType.INSTANCE
+						.getDefaultExtension(), "");
+			}
+		});
+	}
 
-  public void disposeComponent() {
-  }
+	public void disposeComponent()
+	{
+	}
 
-  public static ClojurePsiManager getInstance(Project project) {
-    return project.getComponent(ClojurePsiManager.class);
-  }
+	public static ClojurePsiManager getInstance(Project project)
+	{
+		return project.getComponent(ClojurePsiManager.class);
+	}
 
-  public PsiFile getDummyFile() {
-    return myDummyFile;
-  }
+	public PsiFile getDummyFile()
+	{
+		return myDummyFile;
+	}
 }
