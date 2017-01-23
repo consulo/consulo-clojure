c0fb3ed5ba8ffbe402a8c02acec39f7b02cb2d0f
VISTALL <vistall.valeriy@gmail.com>
2017-01-23 16:57:38 +0300

    compilation

--- src/org/jetbrains/plugins/clojure/compiler/ClojureCompilerSettings.java
+++ src/org/jetbrains/plugins/clojure/compiler/ClojureCompilerSettings.java
@@ -1,34 +1,41 @@
 package org.jetbrains.plugins.clojure.compiler;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.clojure.model.impl.JpsClojureCompilerSettingsState;
 import org.jetbrains.jps.clojure.model.impl.JpsClojureModelSerializerExtension;
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StorageScheme;
+import com.intellij.openapi.project.Project;
 import com.intellij.util.xmlb.XmlSerializerUtil;
-import consulo.lombok.annotations.ProjectService;
 
 /**
  * @author ilyas
  */
-@State(
-  name = JpsClojureModelSerializerExtension.CLOJURE_COMPILER_SETTINGS_COMPONENT_NAME,
-  storages = {
-    @Storage(id = "default", file = "$PROJECT_FILE$")
-   ,@Storage(id = "dir", file = "$PROJECT_CONFIG_DIR$/" + JpsClojureModelSerializerExtension.CLOJURE_COMPILER_SETTINGS_FILE, scheme = StorageScheme.DIRECTORY_BASED)
-    }
-)
-@ProjectService
-public class ClojureCompilerSettings implements PersistentStateComponent<JpsClojureCompilerSettingsState> {
-  private JpsClojureCompilerSettingsState myState = new JpsClojureCompilerSettingsState();
+@State(name = JpsClojureModelSerializerExtension.CLOJURE_COMPILER_SETTINGS_COMPONENT_NAME, storages = {
+		@Storage(id = "default", file = "$PROJECT_FILE$"),
+		@Storage(id = "dir", file = "$PROJECT_CONFIG_DIR$/" + JpsClojureModelSerializerExtension.CLOJURE_COMPILER_SETTINGS_FILE, scheme = StorageScheme.DIRECTORY_BASED)
+})
+public class ClojureCompilerSettings implements PersistentStateComponent<JpsClojureCompilerSettingsState>
+{
+	@NotNull
+	public static ClojureCompilerSettings getInstance(@NotNull Project project)
+	{
+		return ServiceManager.getService(project, ClojureCompilerSettings.class);
+	}
 
-  public JpsClojureCompilerSettingsState getState() {
-    return myState;
-  }
-                
-  public void loadState(JpsClojureCompilerSettingsState state) {
-    XmlSerializerUtil.copyBean(state, myState);
-  }
+	private JpsClojureCompilerSettingsState myState = new JpsClojureCompilerSettingsState();
+
+	public JpsClojureCompilerSettingsState getState()
+	{
+		return myState;
+	}
+
+	public void loadState(JpsClojureCompilerSettingsState state)
+	{
+		XmlSerializerUtil.copyBean(state, myState);
+	}
 
 }
